988a3c6d3c1b9fd1e02b4cd7299305cc49d35e8d
Salik <salik.ansari6@gmail.com>
2021-02-20 13:44:05 +0530

    updated route component to fix routing bugs and to support client side route using custom link component in future

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,15 +3,11 @@ import SearchResults from './SearchResults/SearchResults';
 import './Dashboard.css'
 import { LoginContext } from '../../contexts/LoginContext';
 const Dashboard = () => {
-
     const {setIsLoggedIn,goToDashboard} = useContext(LoginContext)
 
-
     useEffect(() =>{
-        setIsLoggedIn(true)
+        setIsLoggedIn(true)        
     },[setIsLoggedIn,goToDashboard])
-
-
 
     return (
         <div className={ goToDashboard? "dashboard":'' }>
